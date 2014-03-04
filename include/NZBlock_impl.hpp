58d6311119f9ce235e6bcdea37c25c16dccbd1eb
Mathias <mjacquelin@lbl.gov>
2014-03-04 14:28:44 -0800

    missing file for LBLOCKs

---
+++ include/NZBlock_impl.hpp
@@ -0,0 +1,29 @@
+/// @file NZBlock.hpp
+/// @brief 
+/// @author Mathias Jacquelin
+/// @date 2014-03-03
+#ifndef _NZBLOCK_IMPL_HPP_ 
+#define _NZBLOCK_IMPL_HPP_
+
+namespace LIBCHOLESKY{
+
+
+  template <typename T> NZBlock<T>::NZBlock(T * apNzval, Int aiNzcnt, Int aiGIndex, Int aiLIndex){
+    if(apNzval==NULL){
+      abort();
+    }
+    
+    this->storage_.resize(sizeof(Int)*2 + sizeof(T)*aiNzcnt);
+    this->pGIndex_ = reinterpret_cast<Int*>(&storage_[0]);
+    this->pLIndex_ = reinterpret_cast<Int*>(&storage_[0])+1;
+    this->pNzval_= reinterpret_cast<T*>(this->pLIndex_+1);
+    this->iNzcnt_ = aiNzcnt;
+    *this->pGIndex_ = aiGIndex;
+    *this->pLIndex_ = aiLIndex;
+    std::copy(apNzval, apNzval+aiNzcnt, this->pNzval_);
+  }
+
+
+}
+
+#endif
