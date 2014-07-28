05a6cc49ff8da09e433da10f59a6e1f69a9a789c
Kevin Carlson <kcarlson2915@gmail.com>
2014-07-28 15:50:14 -0700

    More work on the delegate ordering algorithm

--- delegate/delegateOrdering.py
+++ delegate/delegateOrdering.py
@@ -1,5 +1,5 @@
 #Kevin Carlson
-#Python Call Cost Interface
+#Python Delegate Ordering Algorithm
 #LBL Summer 2014
 
 import sys
@@ -7,7 +7,7 @@ import ctypes
 
 #Read the Adjacency File from the command line
 adjacencyFile = bytes(sys.argv[1], 'utf-8')
-permutation = sys.argv[2]
+inputPermutation = sys.argv[2]
 
 #Import the C Functions from util.cpp and set the arguments and return types.
 utilLib = ctypes.CDLL('/Users/kevin/Desktop/LBLrepo/ngchol/delegate/sharedLib.so')
@@ -39,18 +39,75 @@ cN = ctypes.pointer(ctypes.c_int(0))
 cNnz = ctypes.pointer(ctypes.c_int(0))
 ReadAdjacency(cAdjacencyFile, cXAdj, cAdj, cN, cNnz)
 
-#Set up variables and call GetCost.
-XAdj = cXAdj.contents
+#Set up variables and call GetCost for the input permutation.
+xAdj = cXAdj.contents
 Adj = cAdj.contents
 N = cN.contents
 Nnz = cNnz.contents
-perm = (ctypes.c_int * N.value)()
-for i in range(len(permutation.split())):
-    perm[i] = int(permutation.split()[i])
-cost = GetCost(N, Nnz, XAdj, Adj, perm)
-print(cost)
+bestPerm = (ctypes.c_int * N.value)()
+currentPerm = (ctypes.c_int * N.value)()
+for i in range(len(inputPermutation.split())):
+    bestPerm[i] = int(inputPermutation.split()[i])
+    currentPerm[i] = int(inputPermutation.split()[i])
+inputCost = GetCost(N, Nnz, xAdj, Adj, bestPerm)
 
-#Free the Adj and XAdj Arrays and exit.
-utilLib.freeIntPointer(XAdj)
-utilLib.freeIntPointer(Adj)
-exit(0)
+#Set up Python Lists for the Adj and xAdj arays
+pyAdj = []
+for i in range(Nnz.value):
+    pyAdj.append(Adj[i]) 
+
+pyXAdj = []
+for i in range(N.value + 1):
+    pyXAdj.append(xAdj[i])
+
+
+#Returns a list of all of the neightbors of 
+#Value within depth range.
+def getNeighbors(value, depth=1):
+    start = pyXAdj[value - 1] - 1
+    end = pyXAdj[value]-1 
+    return pyAdj[start:end] 
+
+
+bestCost = float("inf")
+
+
+def permute():
+    global bestCost
+    global bestPerm
+    global CurrentPerm
+    for start in range(N.value):
+        bestIndex = start
+        currentIndex = start
+        while currentIndex < N.value:
+            temp = currentPerm[currentIndex]
+            currentPerm[currentIndex] = currentPerm[start]
+            currentPerm[start] = temp
+            nextCost = GetCost(N, Nnz, xAdj, Adj, currentPerm)
+            if nextCost < bestCost:
+                bestIndex = currentIndex
+                bestCost = nextCost
+            temp = currentPerm[currentIndex]
+            currentPerm[currentIndex] = currentPerm[start]
+            currentPerm[start] = temp
+            currentIndex += 1
+        swap = currentPerm[bestIndex]
+        currentPerm[bestIndex] = currentPerm[start]
+        bestPerm[bestIndex] = bestPerm[start]
+        currentPerm[start] = swap
+        bestPerm[start] = swap
+    return bestCost
+
+
+
+
+def legalPerm(permutation):
+    testSet = set()
+    for i in range(N.value):
+        testSet.add(permutation[i])
+    if len(testSet) == N.value:
+        return True
+    return False
+    
+
+
