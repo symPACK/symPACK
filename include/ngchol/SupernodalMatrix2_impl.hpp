symPACK/symPACK

commit d586af59c255563a878908cc33677dc8b9a9d3aa

going to try on edison

include/ngchol/SupernodalMatrix2_impl.hpp
@@ -932,15 +932,16 @@ TIMER_STOP(ADVANCE_OUTGOING_COMM);
 
 
   template <typename T> SparseMatrixStructure SupernodalMatrix2<T>::GetLocalStructure() const {
-    return Local_;
+    return *Local_;
   }
 
   template <typename T> SparseMatrixStructure SupernodalMatrix2<T>::GetGlobalStructure(){
     if(isGlobStructAllocated_){
-      Local_.ToGlobal(Global_,CommEnv_->MPI_GetComm());
+      Global_=new SparseMatrixStructure();
+      Local_->ToGlobal(*Global_,CommEnv_->MPI_GetComm());
       isGlobStructAllocated_= true;
     }
-    return Global_;
+    return *Global_;
   }
 
 
@@ -1068,16 +1069,18 @@ void SupernodalMatrix2<T>::Dump(){
 
 
     iSize_ = pMat.size;
-    Local_ = pMat.GetLocalStructure();
+    Local_ = new SparseMatrixStructure();
+    Global_ = new SparseMatrixStructure();
+    *Local_ = pMat.GetLocalStructure();
 
-    Local_.ToGlobal(Global_,CommEnv_->MPI_GetComm());
-    Global_.ExpandSymmetric();
+    Local_->ToGlobal(*Global_,CommEnv_->MPI_GetComm());
+    Global_->ExpandSymmetric();
     isGlobStructAllocated_ = true;
 
 logfileptr->OFS()<<"Matrix expanded"<<endl;
 
     //Create an Ordering object to hold the permutation
-    Order_.SetStructure(Global_);
+    Order_.SetStructure(*Global_);
 logfileptr->OFS()<<"Structure set"<<endl;
 
     switch(options_.ordering){
@@ -1094,11 +1097,11 @@ logfileptr->OFS()<<"Structure set"<<endl;
     }
 logfileptr->OFS()<<"Ordering done"<<endl;
 
-    ETree_.ConstructETree(Global_,Order_);
+    ETree_.ConstructETree(*Global_,Order_);
     ETree_.PostOrderTree(Order_);
 logfileptr->OFS()<<"ETREE done"<<endl;
     IntNumVec cc,rc;
-    Global_.GetLColRowCount(ETree_,Order_,cc,rc);
+    Global_->GetLColRowCount(ETree_,Order_,cc,rc);
     ETree_.SortChildren(cc,Order_);
 
 #ifdef _DEBUG_
@@ -1114,17 +1117,17 @@ logfileptr->OFS()<<"ETREE done"<<endl;
       cout<<"Flops: "<<flops<<endl;
     }
 
-    Global_.FindSupernodes(ETree_,Order_,cc,SupMembership_,Xsuper_,options.maxSnode);
+    Global_->FindSupernodes(ETree_,Order_,cc,SupMembership_,Xsuper_,options.maxSnode);
 
 logfileptr->OFS()<<"Supernodes found"<<endl;
 
 #ifdef RELAXED_SNODE
-    Global_.RelaxSupernodes(ETree_, cc,SupMembership_, Xsuper_, options.maxSnode );
+    Global_->RelaxSupernodes(ETree_, cc,SupMembership_, Xsuper_, options.maxSnode );
 logfileptr->OFS()<<"Relaxation done"<<endl;
-    Global_.SymbolicFactorizationRelaxed(ETree_,Order_,cc,Xsuper_,SupMembership_,xlindx_,lindx_);
+    Global_->SymbolicFactorizationRelaxed(ETree_,Order_,cc,Xsuper_,SupMembership_,xlindx_,lindx_);
 
 #else
-    Global_.SymbolicFactorization(ETree_,Order_,cc,Xsuper_,SupMembership_,xlindx_,lindx_);
+    Global_->SymbolicFactorization(ETree_,Order_,cc,Xsuper_,SupMembership_,xlindx_,lindx_);
 #endif
 
 logfileptr->OFS()<<"Symbfact done"<<endl;
@@ -1132,7 +1135,7 @@ logfileptr->OFS()<<"Symbfact done"<<endl;
 #ifdef REFINED_SNODE
     IntNumVec permRefined;
     //    IntNumVec newPerm(Size());
-    Global_.RefineSupernodes(ETree_,Order_, SupMembership_, Xsuper_, xlindx_, lindx_, permRefined);
+    Global_->RefineSupernodes(ETree_,Order_, SupMembership_, Xsuper_, xlindx_, lindx_, permRefined);
 
     //      Perm_.Resize(Size());
     //      for(Int i =0; i<Perm_.m();++i){
@@ -1496,22 +1499,22 @@ isLindxAllocated_=true;
       ExpA.Local_.colptr.Resize(numColLocal+1);
 
       for( Int i = 0; i < numColLocal + 1; i++ ){
-        ExpA.Local_.colptr[i] = Global_.expColptr[iam * numColFirst+i] - Global_.expColptr[iam * numColFirst] + 1;
+        ExpA.Local_.colptr[i] = Global_->expColptr[iam * numColFirst+i] - Global_->expColptr[iam * numColFirst] + 1;
       }
 
       ExpA.Local_.nnz = ExpA.Local_.colptr[numColLocal] - ExpA.Local_.colptr[0];
 
       ExpA.Local_.rowind.Resize(ExpA.Local_.nnz);
 
-      Int globalColBeg = Global_.expColptr[iam*numColFirst];
-      std::copy(&Global_.expRowind[globalColBeg-1],&Global_.expRowind[globalColBeg-1]+ExpA.Local_.nnz,ExpA.Local_.rowind.Data());
+      Int globalColBeg = Global_->expColptr[iam*numColFirst];
+      std::copy(&Global_->expRowind[globalColBeg-1],&Global_->expRowind[globalColBeg-1]+ExpA.Local_.nnz,ExpA.Local_.rowind.Data());
       ExpA.nzvalLocal.Resize(ExpA.Local_.nnz);
 
 #ifdef _DEBUG_
-      logfileptr->OFS()<<"Global_.colptr: "<<Global_.colptr<<endl;
-      logfileptr->OFS()<<"Global_.rowind: "<<Global_.rowind<<endl;
-      logfileptr->OFS()<<"Global_.expColptr: "<<Global_.expColptr<<endl;
-      logfileptr->OFS()<<"Global_.expRowind: "<<Global_.expRowind<<endl;
+      logfileptr->OFS()<<"Global_.colptr: "<<Global_->colptr<<endl;
+      logfileptr->OFS()<<"Global_.rowind: "<<Global_->rowind<<endl;
+      logfileptr->OFS()<<"Global_.expColptr: "<<Global_->expColptr<<endl;
+      logfileptr->OFS()<<"Global_.expRowind: "<<Global_->expRowind<<endl;
       logfileptr->OFS()<<"ExpA.colptr: "<<ExpA.Local_.colptr<<endl;
       logfileptr->OFS()<<"ExpA.rowind: "<<ExpA.Local_.rowind<<endl;
       logfileptr->OFS()<<"pMat.colptr: "<<pMat.Local_.colptr<<endl;
@@ -1658,7 +1661,7 @@ isLindxAllocated_=true;
 
             if(iam == iDest){
               if(iam != iOwnerCol){
-                Int nrows = Global_.expColptr[orig_col] - Global_.expColptr[orig_col-1];
+                Int nrows = Global_->expColptr[orig_col] - Global_->expColptr[orig_col-1];
                 size_t & recv_bytes = recv_map[iOwnerCol].first;
                 recv_bytes += sizeof(Int)+sizeof(Int)+nrows*(sizeof(Int) + sizeof(T));
               }
@@ -1677,6 +1680,7 @@ isLindxAllocated_=true;
         LocalSupernodes_.reserve(snodeCount);
 
 
+        
         for(Int I=1;I<Xsuper_.m();I++){
 
           Int iDest = this->Mapping_->Map(I-1,I-1);
@@ -1697,7 +1701,24 @@ isLindxAllocated_=true;
             globToLocSnodes_.Insert(snode_inter);
 #endif
 
-            LocalSupernodes_.push_back( new SuperNode2<T>(I,fc,lc,iHeight,iSize_));
+                  //count number of contiguous blocks (at least one diagonal block)
+                  Int nzBlockCnt = 1;
+                  Idx32 iPrevRow = lindx_(fi-1)-1;
+                  for(Idx64 idx = fi; idx<=li;idx++){
+                    Idx32 iRow = lindx_(idx-1);
+    
+                    //enforce the first block to be a square diagonal block
+                    if(nzBlockCnt==1 && iRow>fc/*lindx_(fi-1)*/+iWidth-1){
+                      nzBlockCnt++;
+                    }
+                    else if(iRow!=iPrevRow+1){
+                      nzBlockCnt++;
+                    }
+
+                    iPrevRow=iRow;
+                  }
+
+            LocalSupernodes_.push_back( new SuperNode2<T>(I,fc,lc,iHeight,iSize_,nzBlockCnt));
           }
         }
 
@@ -1906,7 +1927,14 @@ isLindxAllocated_=true;
             Idx64 li = xlindx_(I)-1;
             Int iHeight = li-fi+1;
 
+#ifndef ITREE2
+            globToLocSnodes_.push_back(I-1);
+#else 
+            ITree::Interval snode_inter = { I, I, LocalSupernodes_.size() };
+            globToLocSnodes_.Insert(snode_inter);
+#endif
 
+            LocalSupernodes_.push_back( new SuperNode2<T>(I,fc,lc,iHeight,iSize_));
             SuperNode2<T> & snode = *snodeLocal(I);
 
             if(snode.NZBlockCnt()==0){
@@ -2065,7 +2093,12 @@ isLindxAllocated_=true;
 
 
 
-
+    gdb_lock();
+    xlindx_.Clear();
+    lindx_.Clear();
+    delete Local_;
+    delete Global_;
+    gdb_lock();
 
 
 
@@ -2085,6 +2118,8 @@ isLindxAllocated_=true;
     CommEnv_=NULL;
     Mapping_ = NULL;
     scheduler_ = NULL;
+    Local_=NULL;
+    Global_=NULL;
     isGlobStructAllocated_ = false;
     isXlindxAllocated_=false;
     isLindxAllocated_=false;
@@ -2094,6 +2129,8 @@ isLindxAllocated_=true;
     CommEnv_ = NULL;
     Mapping_ = NULL;
     scheduler_ = NULL;
+    Local_=NULL;
+    Global_=NULL;
     isGlobStructAllocated_ = false;
     isXlindxAllocated_=false;
     isLindxAllocated_=false;
@@ -2125,6 +2162,14 @@ isLindxAllocated_=true;
 
     if(CommEnv_!=NULL){
       delete CommEnv_;
+    }
+
+    if(Local_!=NULL){
+      delete Local_;
+    }
+
+    if(Global_!=NULL){
+      delete Global_;
     }
   }
 

driver/Results/pwtk/07-27-2015/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL.e3334279
@@ -1,183 +1,0 @@
--------------121-----------
--------------/global/homes/m/mjac/ngchol_scratch/driver/run_sparse_pull-----------
-aprun -n 121 /global/homes/m/mjac/ngchol_scratch/driver/run_sparse_pull -in /global/homes/m/mjac/pexsi_project/UFL-Matrices/pwtk/pwtk.rb  -inf CSC -map ModWrap2D -b 50 -ir -1 -is 100 -fb 1 -ordering AMD -lb SUBCUBE -scheduler DL
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL/run_sparse_pull/1
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.263943
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.323926
-Allocation time: 2.30068
-Starting Factorization
-Total volume of communication: 2125021552
-Total number of messages: 17325
-Factorization time: 0.540269
-Starting solve
-Solve time: 0.141885
-Norm of residual after SPCHOL is 1.67579e-15
-Application 14280364 resources: utime ~553s, stime ~426s, Rss ~177668, inblocks ~41104291, outblocks ~1095587
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL/run_sparse_pull/2
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.419182
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.61057
-Allocation time: 2.54729
-Starting Factorization
-Total volume of communication: 2125021456
-Total number of messages: 17325
-Factorization time: 0.54107
-Starting solve
-Solve time: 0.142521
-Norm of residual after SPCHOL is 1.6973e-15
-Application 14280366 resources: utime ~600s, stime ~423s, Rss ~177820, inblocks ~41104314, outblocks ~1095585
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL/run_sparse_pull/3
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.416466
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.23157
-Allocation time: 2.29611
-Starting Factorization
-Total volume of communication: 2125021520
-Total number of messages: 17325
-Factorization time: 0.541637
-Starting solve
-Solve time: 0.142463
-Norm of residual after SPCHOL is 1.65909e-15
-Application 14280368 resources: utime ~551s, stime ~424s, Rss ~177864, inblocks ~41104335, outblocks ~1095590
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL/run_sparse_pull/4
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.17961
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.67795
-Allocation time: 2.61866
-Starting Factorization
-Total volume of communication: 2125023392
-Total number of messages: 17325
-Factorization time: 0.54703
-Starting solve
-Solve time: 0.141668
-Norm of residual after SPCHOL is 1.67409e-15
-Application 14280370 resources: utime ~603s, stime ~423s, Rss ~177864, inblocks ~41104222, outblocks ~1095587
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL/run_sparse_pull/5
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.695532
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.715499
-Allocation time: 2.78002
-Starting Factorization
-Total volume of communication: 2125021936
-Total number of messages: 17325
-Factorization time: 0.55489
-Starting solve
-Solve time: 0.14238
-Norm of residual after SPCHOL is 1.69489e-15
-Application 14280375 resources: utime ~622s, stime ~424s, Rss ~177672, inblocks ~41104270, outblocks ~1095585
--------------/global/homes/m/mjac/ngchol_scratch/driver/run_sparse_fanboth_upcxx-----------
-aprun -n 121 /global/homes/m/mjac/ngchol_scratch/driver/run_sparse_fanboth_upcxx -in /global/homes/m/mjac/pexsi_project/UFL-Matrices/pwtk/pwtk.rb  -inf CSC -map ModWrap2D -b 50 -ir -1 -is 100 -fb 1 -ordering AMD -lb SUBCUBE -scheduler DL
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL/run_sparse_fanboth_upcxx/1
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.237934
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.486023
-Allocation time: 2.38258
-Starting Factorization
-Factorization time: 0.828504
-Starting solve
-Solve time: 0.12654
-Norm of residual after SPCHOL is 1.65507e-15
-Application 14280377 resources: utime ~538s, stime ~215s, Rss ~176840, inblocks ~41118989, outblocks ~1074717
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL/run_sparse_fanboth_upcxx/2
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.411488
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.33935
-Allocation time: 2.19944
-Starting Factorization
-Factorization time: 0.829515
-Starting solve
-Solve time: 0.128121
-Norm of residual after SPCHOL is 1.64043e-15
-Application 14280380 resources: utime ~527s, stime ~214s, Rss ~176744, inblocks ~41118900, outblocks ~1074720
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL/run_sparse_fanboth_upcxx/3
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.559357
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.339014
-Allocation time: 2.2839
-Starting Factorization
-Factorization time: 0.826583
-Starting solve
-Solve time: 0.126654
-Norm of residual after SPCHOL is 1.65292e-15
-Application 14280383 resources: utime ~539s, stime ~212s, Rss ~176992, inblocks ~41118568, outblocks ~1074718
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL/run_sparse_fanboth_upcxx/4
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.32564
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.192318
-Allocation time: 2.194
-Starting Factorization
-Factorization time: 0.824149
-Starting solve
-Solve time: 0.127236
-Norm of residual after SPCHOL is 1.64921e-15
-Application 14280388 resources: utime ~522s, stime ~214s, Rss ~176992, inblocks ~41118713, outblocks ~1074716
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL/run_sparse_fanboth_upcxx/5
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.455317
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 1.21013
-Allocation time: 3.14617
-Starting Factorization
-Factorization time: 0.824734
-Starting solve
-Solve time: 0.127116
-Norm of residual after SPCHOL is 1.64468e-15
-Application 14280393 resources: utime ~634s, stime ~204s, Rss ~176656, inblocks ~41118676, outblocks ~1074717
-
- + --------------------------------------------------------------------------
- +        Job name: NGCHOL_pwtk.rb_121_FB_1_ModWrap2D_50_DL
- +          Job Id: 3334279.edique02
- +          System: edison
- +     Queued Time: Mon Jul 27 16:00:31 2015
- +      Start Time: Mon Jul 27 16:13:10 2015
- + Completion Time: Mon Jul 27 16:15:53 2015
- +            User: mjac
- +        MOM Host: nid02434
- +           Queue: debug
- +  Req. Resources: mppnodect=6,mppnppn=24,mppwidth=121,walltime=00:30:00
- +  Used Resources: cput=00:00:16,energy_used=0,mem=10768kb,vmem=163372kb,walltime=00:02:45
- +     Acct String: mp127
- +   PBS_O_WORKDIR: /scratch1/scratchdirs/mjac/ngchol/driver
- +     Submit Args: tmp.pbs
- + --------------------------------------------------------------------------
-

driver/Results/pwtk/07-27-2015/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL.e3333926
@@ -1,183 +1,0 @@
--------------128-----------
--------------/global/homes/m/mjac/ngchol_scratch/driver/run_sparse_pull-----------
-aprun -n 128 /global/homes/m/mjac/ngchol_scratch/driver/run_sparse_pull -in /global/homes/m/mjac/pexsi_project/UFL-Matrices/pwtk/pwtk.rb  -inf CSC -map Col2D -b 50 -ir -1 -is 100 -fb 1 -ordering AMD -lb SUBCUBE -scheduler DL
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL/run_sparse_pull/1
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 1.10605
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.198587
-Allocation time: 2.30799
-Starting Factorization
-Total volume of communication: 1774365072
-Total number of messages: 7093
-Factorization time: 0.573863
-Starting solve
-Solve time: 0.143047
-Norm of residual after SPCHOL is 1.63635e-15
-Application 14279548 resources: utime ~698s, stime ~448s, Rss ~177792, inblocks ~43465235, outblocks ~1114330
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL/run_sparse_pull/2
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.277894
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 1.90335
-Allocation time: 4.01155
-Starting Factorization
-Total volume of communication: 1774365072
-Total number of messages: 7093
-Factorization time: 0.567388
-Starting solve
-Solve time: 0.142119
-Norm of residual after SPCHOL is 1.64595e-15
-Application 14279555 resources: utime ~883s, stime ~439s, Rss ~177712, inblocks ~43465259, outblocks ~1114327
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL/run_sparse_pull/3
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 1.66915
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.194099
-Allocation time: 2.29709
-Starting Factorization
-Total volume of communication: 1774365072
-Total number of messages: 7093
-Factorization time: 0.566616
-Starting solve
-Solve time: 0.143372
-Norm of residual after SPCHOL is 1.65769e-15
-Application 14279559 resources: utime ~732s, stime ~451s, Rss ~177752, inblocks ~43465284, outblocks ~1114327
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL/run_sparse_pull/4
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 1.1767
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.180631
-Allocation time: 2.33528
-Starting Factorization
-Total volume of communication: 1774365072
-Total number of messages: 7093
-Factorization time: 0.570875
-Starting solve
-Solve time: 0.143706
-Norm of residual after SPCHOL is 1.64824e-15
-Application 14279570 resources: utime ~666s, stime ~442s, Rss ~177672, inblocks ~43465287, outblocks ~1114330
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL/run_sparse_pull/5
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.676139
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.447251
-Allocation time: 2.55683
-Starting Factorization
-Total volume of communication: 1774365072
-Total number of messages: 7093
-Factorization time: 0.568241
-Starting solve
-Solve time: 0.144098
-Norm of residual after SPCHOL is 1.66354e-15
-Application 14279572 resources: utime ~664s, stime ~440s, Rss ~177696, inblocks ~43465347, outblocks ~1114329
--------------/global/homes/m/mjac/ngchol_scratch/driver/run_sparse_fanboth_upcxx-----------
-aprun -n 128 /global/homes/m/mjac/ngchol_scratch/driver/run_sparse_fanboth_upcxx -in /global/homes/m/mjac/pexsi_project/UFL-Matrices/pwtk/pwtk.rb  -inf CSC -map Col2D -b 50 -ir -1 -is 100 -fb 1 -ordering AMD -lb SUBCUBE -scheduler DL
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL/run_sparse_fanboth_upcxx/1
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.552074
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.419713
-Allocation time: 2.42809
-Starting Factorization
-Factorization time: 0.756525
-Starting solve
-Solve time: 0.122918
-Norm of residual after SPCHOL is 1.62849e-15
-Application 14279577 resources: utime ~599s, stime ~225s, Rss ~176780, inblocks ~43490529, outblocks ~1093835
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL/run_sparse_fanboth_upcxx/2
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.391398
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.198838
-Allocation time: 2.21626
-Starting Factorization
-Factorization time: 0.75872
-Starting solve
-Solve time: 0.123189
-Norm of residual after SPCHOL is 1.643e-15
-Application 14279590 resources: utime ~557s, stime ~224s, Rss ~176740, inblocks ~43491712, outblocks ~1093840
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL/run_sparse_fanboth_upcxx/3
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.459858
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.224313
-Allocation time: 2.22391
-Starting Factorization
-Factorization time: 0.731723
-Starting solve
-Solve time: 0.12317
-Norm of residual after SPCHOL is 1.6111e-15
-Application 14279597 resources: utime ~555s, stime ~230s, Rss ~176896, inblocks ~43490067, outblocks ~1093824
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL/run_sparse_fanboth_upcxx/4
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.402322
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.180205
-Allocation time: 2.25142
-Starting Factorization
-Factorization time: 0.754276
-Starting solve
-Solve time: 0.122872
-Norm of residual after SPCHOL is 1.60863e-15
-Application 14279599 resources: utime ~567s, stime ~225s, Rss ~176788, inblocks ~43491066, outblocks ~1093832
-/global/homes/m/mjac/ngchol_scratch/experiments/NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL/run_sparse_fanboth_upcxx/5
-Matrix order is 217918
-Starting spGEMM
-spGEMM time: 0.366359
-Starting allocation
-Flops: 4.25157e+10
-Subtree to subcube mapping used
-Time for expanding matrix to asymmetric: 0.251693
-Allocation time: 2.18683
-Starting Factorization
-Factorization time: 0.728537
-Starting solve
-Solve time: 0.123765
-Norm of residual after SPCHOL is 1.61872e-15
-Application 14279603 resources: utime ~556s, stime ~232s, Rss ~176968, inblocks ~43491152, outblocks ~1093827
-
- + --------------------------------------------------------------------------
- +        Job name: NGCHOL_pwtk.rb_128_FB_1_Col2D_50_DL
- +          Job Id: 3333926.edique02
- +          System: edison
- +     Queued Time: Mon Jul 27 15:12:12 2015
- +      Start Time: Mon Jul 27 15:26:41 2015
- + Completion Time: Mon Jul 27 15:29:48 2015
- +            User: mjac
- +        MOM Host: nid00898
- +           Queue: debug
- +  Req. Resources: mppnodect=6,mppnppn=24,mppwidth=128,walltime=00:30:00
- +  Used Resources: cput=00:00:16,energy_used=0,mem=10612kb,vmem=163768kb,walltime=00:03:08
- +     Acct String: mp127
- +   PBS_O_WORKDIR: /scratch1/scratchdirs/mjac/ngchol/driver
- +     Submit Args: tmp.pbs
- + --------------------------------------------------------------------------
-

commit c145ed59f1b8c6dacbf0e4fd2bd59ad6e31d07c0

fixed bug introduced in d586af59c255563a878908cc33677dc8b9a9d3aa

include/ngchol/SupernodalMatrix2_impl.hpp
@@ -1927,14 +1927,14 @@ isLindxAllocated_=true;
             Idx64 li = xlindx_(I)-1;
             Int iHeight = li-fi+1;
 
-#ifndef ITREE2
-            globToLocSnodes_.push_back(I-1);
-#else 
-            ITree::Interval snode_inter = { I, I, LocalSupernodes_.size() };
-            globToLocSnodes_.Insert(snode_inter);
-#endif
-
-            LocalSupernodes_.push_back( new SuperNode2<T>(I,fc,lc,iHeight,iSize_));
+//#ifndef ITREE2
+//            globToLocSnodes_.push_back(I-1);
+//#else 
+//            ITree::Interval snode_inter = { I, I, LocalSupernodes_.size() };
+//            globToLocSnodes_.Insert(snode_inter);
+//#endif
+//
+//            LocalSupernodes_.push_back( new SuperNode2<T>(I,fc,lc,iHeight,iSize_));
             SuperNode2<T> & snode = *snodeLocal(I);
 
             if(snode.NZBlockCnt()==0){