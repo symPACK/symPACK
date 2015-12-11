668c9d9ec10ab93f6d404f2fb7293aeb0792209b
Mathias <mjacquelin@lbl.gov>
2015-12-11 09:45:27 -0800

    latest script

--- experiments/mapping_cost_impact.sh
+++ experiments/mapping_cost_impact.sh
@@ -1,36 +1,44 @@
 # !/bin/sh
 
 ofile=tmp
-problem="../utilities/16x16x16_27.rb"
+#problem="../utilities/16x16x16_7.rb"
+#problem="../utilities/16x16x16_27.rb"
 #problem="../utilities/24x24x24_27.rb"
+problem="../utilities/24x24x24_7.rb"
+#problem="../utilities/3x3x3_27.rb"
 b=20
 #b=20
 #b=40
 LB=NOLB
-ordering=SCOTCH
-#ordering=METIS
-#ordering=NDBOX
-#ordering=MMD
-#ordering=AMD
 
-export GASNET_MAX_SEGSIZE=512M
+export GASNET_MAX_SEGSIZE=2G
+orderings=( "SCOTCH" "NDBOX" "METIS" "MMD" "AMD" )
+mappings=( "MODWRAP2D" "COL2D" "ROW2D" )
+mappings=( "COL2D" )
 
-#for p in 2 4 8 16 24 32 64
-#for p in 2 4 8 16 24 64 81 100 169
-for p in 81
+for ordering in "${orderings[@]}"
 do 
-  #for map in "ROW2D" "COL2D" "MODWRAP2D"
-  for map in "COL2D" i
+#for p in 2 4 8 16 24 32 64
+#for p in 2 4 8 16 24 64 100 169
+#for p in 2 4 8 16 24 64 100 169
+#for p in 64
+#for p in 2 4 8 16 24 64 100 169
+for p in 4
+do 
+  #for map in "COL2D" 
+  for map in "${mappings[@]}"
   do
-    echo -n "$p, $b, $map, " >> $ofile;
+    echo -n "$p, $b, $ordering, $map, " >> $ofile;
     mpirun -n $p ./run_sparse_pull -in $problem -inf HARWELL_BOEING -map $map -b $b -ir 0 -is 0 -fb 1 \
     -ordering $ordering -lb $LB -scheduler DL -relax -1 \
     | grep -e "Total\|NNZ" \
-    | awk '{print $5","}' | sed -e '{
+    | awk '{print $5","}' | tr '\n' ' ' | sed -e '{
 N
 N
 N
 s/\n/ /g
 }' >> $ofile
+    echo " " >> $ofile
   done
+done
 done
