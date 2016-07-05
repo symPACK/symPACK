eab6eaa58f244db6e9422b873f9cceefbbe5c00a
Mathias Jacquelin <mjacquelin@lbl.gov>
2016-07-05 01:12:15 -0700

    merging

--- cmake/upcxx.cmake
+++ cmake/upcxx.cmake
@@ -13,7 +13,7 @@ ExternalProject_Add(${UPCXX_NAME}
 ExternalProject_Add_Step(${UPCXX_NAME} bootstrap
 DEPENDEES patch update patch download
 DEPENDERS configure
-COMMAND <SOURCE_DIR>/Bootstrap.sh 
+COMMAND libtoolize COMMAND <SOURCE_DIR>/Bootstrap.sh 
 WORKING_DIRECTORY <SOURCE_DIR>
 #ALWAYS 1
 COMMENT "Bootstraping the source directory"
