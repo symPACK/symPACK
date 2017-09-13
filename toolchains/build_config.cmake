46f937662e33ebbe15e224bde5f2dd43828e917e
Mathias Jacquelin <mjacquelin@lbl.gov>
2017-09-13 16:40:22 -0700

    Update build_config.cmake

--- toolchains/build_config.cmake
+++ toolchains/build_config.cmake
@@ -1,8 +1,15 @@
 #BLAS and Lapack if not in standard path
-set(BLAS_DIR /path/to/blas)
-set(LAPACK_DIR /path/to/lapack)
+set(BLAS_DIR /path/to/blas CACHE BOOL "...")
+set(LAPACK_DIR /path/to/lapack CACHE BOOL "...")
 set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${BLAS_DIR})
 set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${LAPACK_DIR})
+
+#If BLAS or LAPACK cannot be found, especially when using latest cmake versions,
+#please uncomment the following lines and comment the lines above
+
+#set(BLAS_LIBRARIES /path/to/libblas.a CACHE BOOL "...")
+#set(LAPACK_LIBRARIES /path/to/liblapack.a CACHE BOOL "...")
+
 
 #optional Ordering libraries
 #set(SCOTCH_DIR /path/to/scotch)
