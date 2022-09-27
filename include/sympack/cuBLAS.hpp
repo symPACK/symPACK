8d36cebc6eeaddcf70e3908ea4441a6d266a89c4
Hooninator <jbellavita@berkeley.edu>
2022-09-27 11:18:07 -0700

    BLAS wrappers

--- include/sympack/cuBLAS.hpp
+++ include/sympack/cuBLAS.hpp
@@ -9,17 +9,245 @@
 namespace symPACK {
     /* Test */
     void test_kernel(int);
+    // NOTE: This only contains a subset of the complete BLAS, just the operations used in symPACK are included.
+    // SYRK, TRSM, GEMM, Axpy, Scal, Gemv, Geru, Copy
+    
+    /* ===== LEVEL 1 BLAS ===== */
 
-    /* TRSM wrappers */
-    void cublas_strsm(cublasHandle_t, cublasSideMode_t, cublasFillMode_t, cublasOperation_t, cublasDiagType_t,
+    /* AXPY */
+    cublasStatus_t cublas_saxpy(cublasHandle_t, Int,
+                           const float           *,
+                           const float           *, Int,
+                           float                 *, Int);
+
+    cublasStatus_t cublas_daxpy(cublasHandle_t, Int,
+                           const double           *,
+                           const double           *, Int,
+                           double                 *, Int);
+
+    cublasStatus_t cublas_caxpy(cublasHandle_t, Int,
+                           const cuComplex           *,
+                           const cuComplex           *, Int,
+                           cuComplex                 *, Int);
+
+    cublasStatus_t cublas_zaxpy(cublasHandle_t, Int,
+                           const cuDoubleComplex           *,
+                           const cuDoubleComplex           *, Int,
+                           cuDoubleComplex                 *, Int);
+
+
+    /* COPY */
+    cublasStatus_t cublas_scopy(cublasHandle_t, Int,
+                           const float           *, Int,
+                           float                 *, Int);
+
+    cublasStatus_t cublas_dcopy(cublasHandle_t, Int,
+                           const double           *, Int,
+                           double                 *, Int);
+
+    cublasStatus_t cublas_ccopy(cublasHandle_t, Int,
+                           const cuComplex           *, Int,
+                           cuComplex                 *, Int);
+
+    cublasStatus_t cublas_zcopy(cublasHandle_t, Int,
+                           const cuDoubleComplex           *, Int,
+                           cuDoubleComplex                 *, Int);
+    
+    /* SCAL */
+    cublasStatus_t  cublas_sscal(cublasHandle_t , Int,
+                            const float           *,
+                            float           *, Int);
+
+    cublasStatus_t  cublas_dscal(cublasHandle_t , Int,
+                            const double           *,
+                            double           *, Int);
+
+    cublasStatus_t  cublas_cscal(cublasHandle_t , Int,
+                            const cuComplex           *,
+                            cuComplex           *, Int);
+
+    cublasStatus_t  cublas_csscal(cublasHandle_t , Int,
+                            const float           *,
+                            cuComplex           *, Int);
+
+    cublasStatus_t  cublas_zscal(cublasHandle_t , Int,
+                            const cuDoubleComplex           *,
+                            cuDoubleComplex           *, Int);
+    
+    cublasStatus_t  cublas_zdcal(cublasHandle_t , Int,
+                            const double           *,
+                            cuDoubleComplex           *, Int);
+
+    /* ===== LEVEL 2 BLAS ===== */
+
+    /* GEMV */
+    cublasStatus_t cublas_sgemv(cublasHandle_t , cublasOperation_t ,
+                           Int, Int,
+                           const float           *,
+                           const float           *, Int ,
+                           const float           *, Int ,
+                           const float           *,
+                           float           *, Int );
+
+    cublasStatus_t cublas_dgemv(cublasHandle_t , cublasOperation_t ,
+                           Int, Int,
+                           const double           *,
+                           const double           *, Int ,
+                           const double           *, Int ,
+                           const double           *,
+                           double           *, Int );
+
+    cublasStatus_t cublas_cgemv(cublasHandle_t , cublasOperation_t ,
+                           Int, Int,
+                           const cuComplex           *,
+                           const cuComplex           *, Int ,
+                           const cuComplex           *, Int ,
+                           const cuComplex           *,
+                           cuComplex           *, Int );
+
+    cublasStatus_t cublas_zgemv(cublasHandle_t , cublasOperation_t ,
+                           Int, Int,
+                           const cuDoubleComplex           *,
+                           const cuDoubleComplex           *, Int ,
+                           const cuDoubleComplex           *, Int ,
+                           const cuDoubleComplex           *,
+                           cuDoubleComplex           *, Int );
+
+
+    /* GER */
+    cublasStatus_t  cublas_sger(cublasHandle_t , Int, Int,
+                           const float           *,
+                           const float           *, Int,
+                           const float           *, Int,
+                           float           *, Int);
+
+    cublasStatus_t  cublas_dger(cublasHandle_t , Int, Int,
+                           const double           *,
+                           const double           *, Int,
+                           const double           *, Int,
+                           double           *, Int);
+
+    cublasStatus_t  cublas_cgeru(cublasHandle_t , Int, Int,
+                           const cuComplex           *,
+                           const cuComplex           *, Int,
+                           const cuComplex           *, Int,
+                           cuComplex           *, Int);
+
+    cublasStatus_t  cublas_cgerc(cublasHandle_t , Int, Int,
+                           const cuComplex           *,
+                           const cuComplex           *, Int,
+                           const cuComplex           *, Int,
+                           cuComplex           *, Int);
+
+    cublasStatus_t  cublas_zgeru(cublasHandle_t , Int, Int,
+                           const cuDoubleComplex           *,
+                           const cuDoubleComplex           *, Int,
+                           const cuDoubleComplex           *, Int,
+                           cuDoubleComplex           *, Int);
+
+    cublasStatus_t  cublas_zgerc(cublasHandle_t , Int, Int,
+                           const cuDoubleComplex           *,
+                           const cuDoubleComplex           *, Int,
+                           const cuDoubleComplex           *, Int,
+                           cuDoubleComplex           *, Int);
+
+    /* ===== LEVEL 3 BLAS ===== */
+    
+    /* SYRK */
+    cublasStatus_t cublas_ssyrk(cublasHandle_t,
+                           cublasFillMode_t, cublasOperation_t,
+                           Int, Int,
+                           const float           *,
+                           const float           *, Int ,
+                           const float           *,
+                           float           *, Int);
+
+    cublasStatus_t cublas_dsyrk(cublasHandle_t,
+                           cublasFillMode_t, cublasOperation_t,
+                           Int, Int,
+                           const double           *,
+                           const double           *, Int ,
+                           const double           *,
+                           double           *, Int);
+
+    cublasStatus_t cublas_csyrk(cublasHandle_t,
+                           cublasFillMode_t, cublasOperation_t,
+                           Int, Int,
+                           const cuComplex           *,
+                           const cuComplex           *, Int ,
+                           const cuComplex           *,
+                           cuComplex           *, Int);
+
+    cublasStatus_t cublas_zsyrk(cublasHandle_t,
+                           cublasFillMode_t, cublasOperation_t,
+                           Int, Int,
+                           const cuDoubleComplex           *,
+                           const cuDoubleComplex           *, Int ,
+                           const cuDoubleComplex           *,
+                           cuDoubleComplex           *, Int);
+
+
+    /* GEMM */
+    cublasStatus_t cublas_sgemm(cublasHandle_t,
+                           cublasOperation_t, cublasOperation_t,
+                           Int, Int, Int,
+                           const float           *,
+                           const float           *, Int,
+                           const float           *, Int,
+                           const float           *,
+                           float           *, Int);
+
+    cublasStatus_t cublas_dgemm(cublasHandle_t,
+                           cublasOperation_t, cublasOperation_t,
+                           Int, Int, Int,
+                           const double           *,
+                           const double           *, Int,
+                           const double           *, Int,
+                           const double           *,
+                           double           *, Int);
+    
+    cublasStatus_t cublas_cgemm(cublasHandle_t,
+                           cublasOperation_t, cublasOperation_t,
+                           Int, Int, Int,
+                           const cuComplex           *,
+                           const cuComplex           *, Int,
+                           const cuComplex           *, Int,
+                           const cuComplex           *,
+                           cuComplex           *, Int);
+
+    cublasStatus_t cublas_cgemm(cublasHandle_t,
+                           cublasOperation_t, cublasOperation_t,
+                           Int, Int, Int,
+                           const cuDoubleComplex           *,
+                           const cuDoubleComplex           *, Int,
+                           const cuDoubleComplex           *, Int,
+                           const cuDoubleComplex           *,
+                           cuDoubleComplex           *, Int);
+
+    cublasStatus_t cublas_hgemm(cublasHandle_t,
+                           cublasOperation_t, cublasOperation_t,
+                           Int, Int, Int,
+                           const __half           *,
+                           const __half           *, Int,
+                           const __half           *, Int,
+                           const __half           *,
+                           __half           *, Int);
+
+
+    /* TRSM */
+    cublasStatus_t cublas_strsm(cublasHandle_t, cublasSideMode_t, cublasFillMode_t, cublasOperation_t, cublasDiagType_t,
                         Int, Int, const float *, const float *, Int, float *, Int);
     
-    void cublas_dtrsm(cublasHandle_t, cublasSideMode_t, cublasFillMode_t, cublasOperation_t, cublasDiagType_t,
+    cublasStatus_t cublas_dtrsm(cublasHandle_t, cublasSideMode_t, cublasFillMode_t, cublasOperation_t, cublasDiagType_t,
                         Int, Int, const double *, const double *, Int, double *, Int);
     
-    void cublas_ctrsm(cublasHandle_t, cublasSideMode_t, cublasFillMode_t, cublasOperation_t, cublasDiagType_t,
+    cublasStatus_t cublas_ctrsm(cublasHandle_t, cublasSideMode_t, cublasFillMode_t, cublasOperation_t, cublasDiagType_t,
                         Int, Int, const cuComplex *, const cuComplex *, Int, cuComplex *, Int);
     
-    void cublas_ztrsm(cublasHandle_t, cublasSideMode_t, cublasFillMode_t, cublasOperation_t, cublasDiagType_t,
+    cublasStatus_t cublas_ztrsm(cublasHandle_t, cublasSideMode_t, cublasFillMode_t, cublasOperation_t, cublasDiagType_t,
                         Int, Int, const cuDoubleComplex *, const cuDoubleComplex *, Int, cuDoubleComplex *, Int);
+    
+    template<typename T>
+    cublasStatus_t cublas_ttrsm(cublasHandle_t, cublasSideMode_t, cublasFillMode_t, cublasOperation_t, cublasDiagType_t,
+                        Int, Int, const T *, const T *, Int, T *, Int);
 }
